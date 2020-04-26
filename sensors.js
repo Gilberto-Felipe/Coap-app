6f6884ae4446d8a6cf135f02214e820fb74390de
gil <gramirez25@ucol.mx>
2020-04-25 19:51:34 -0500

    Sensors file OK
    sensors.js gather humidity.js, light.js, and temperature.js in a single file to open just one terminal for the three sensors.

--- sensors.js
+++ sensors.js
@@ -45,6 +45,78 @@ setInterval(() => {
 }, 4000);
 
 
+// light sensor
+let requestLI = coap.request({
+    hostname: 'localhost',
+    pathname: '/sensores/light',
+    method: 'POST'
+})
+
+requestLI.on('response', (res) => {
+    let respuesta = JSON.parse(res.payload.toString());
+    console.log(respuesta.estado)
+})
+
+requestLI.write(JSON.stringify({ activar: true }))
+
+requestLI.end()
+
+
+setInterval(() => {
+
+    let light = getRandomIntInclusive(100,1000)
+
+    let req = coap.request({
+        hostname: 'localhost',
+        pathname: `/sensores/light`,
+        method: 'PUT'
+    })
+
+    req.on('response', (res) => {
+        let respuesta = JSON.parse(res.payload.toString());
+        console.log(respuesta.estado)
+    })
+
+    req.end(JSON.stringify({ value: `${light}` }))
+
+}, 4000);
+
+
+// temperature sensor
+let requestTE = coap.request({
+    hostname: 'localhost',
+    pathname: '/sensores/temperature',
+    method: 'POST'
+})
+
+requestTE.on('response', (res) => {
+    let respuesta = JSON.parse(res.payload.toString());
+    console.log(respuesta.estado)
+})
+
+requestTE.write(JSON.stringify({ activar: true }))
+
+requestTE.end()
+
+
+setInterval(() => {
+
+    let temperature = getRandomIntInclusive(1,100)
+
+    let req = coap.request({
+        hostname: 'localhost',
+        pathname: `/sensores/temperature`,
+        method: 'PUT'
+    })
+
+    req.on('response', (res) => {
+        let respuesta = JSON.parse(res.payload.toString());
+        console.log(respuesta.estado)
+    })
+
+    req.end(JSON.stringify({ value: `${temperature}` }))
+
+}, 4000);
 
 
 
